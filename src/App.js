351e6b7aa01a6897afac8723e44fcd41c8642649
Daniel Vasquez <danielvasquez@Mac-mini-de-Daniel.local>
2021-08-15 22:28:48 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const Loading = () => <p>Cargando...</p>;
+
+const Results = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      {props.results.map((result) => {
+        return <p>{result}</p>;
+      })}
+    </div>
+  );
+};
+
+const state1 = {
+  title: "Hola",
+  results: [1, 2, 3],
+  loading: false,
+};
+
+const state2 = {
+  title: "Hola",
+  results: [1, 2, 3],
+  loading: true,
+};
+
+const state3 = {
+  title: "Hola",
+  results: [4, 2, 6],
+  loading: false,
+};
 
 function App() {
+  const [state, setState] = useState({
+    title: "Hola",
+    results: [1, 2, 3],
+    loading: false,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setState(state1)}>Initial state</button>
+      <button onClick={() => setState(state2)}>Pedir datos a una API</button>
+      <button onClick={() => setState(state3)}>
+        Success y pintar nuevos datos
+      </button>
+
+      {state.loading && <Loading />}
+
+      {!state.loading && (
+        <Results title={state.title} results={state.results} />
+      )}
     </div>
   );
 }
